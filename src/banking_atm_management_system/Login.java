63a3303182e668ae03f5249cfc1617b6d39281c1
Vibhu Gupta <vik6026717961@gmail.com>
2023-07-18 23:02:04 +0530

    co
    new

---
+++ src/banking_atm_management_system/Login.java
@@ -0,0 +1,30 @@
+
+package banking_atm_management_system;
+
+import javax.swing.*;
+import java.awt.*;
+
+
+public class Login  extends JFrame{
+    
+    Login(){
+        
+        setTitle("AUTOMATED TELLER MACHINE");
+        
+        ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("icons/logo.jpg"));
+        Image i2 = i1.getImage().getScaledInstance(100,100 ,Image.SCALE_DEFAULT);
+        ImageIcon i3 = new ImageIcon(i2);
+        JLabel label = new JLabel(i3);
+        add(label);
+        
+       setSize(800 ,480); 
+       setVisible (true);
+       setLocation(350,200);
+    }
+    
+    public static void main(String[] args) {
+        
+        new Login ();
+    }
+    
+}
